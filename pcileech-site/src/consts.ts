771fca8b8aba88dcde9f8d2b0c947098f61a0615
Ramsey McGrath <ramseymcgrath@gmail.com>
2025-08-10 17:12:10 -0400

    add config

---
+++ pcileech-site/src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "PCILeech Firmware Generator";
+export const SITE_DESCRIPTION = "Generate authentic PCIe DMA firmware from real donor hardware with a single command.";
+export const GITHUB_URL = "https://github.com/ramseymcgrath/PCILeechFWGenerator";
+export const DISCORD_URL = "https://discord.gg/your-discord";
+export const AUTHOR_NAME = "Ramsey McGrath";
+export const AUTHOR_EMAIL = "ramsey@example.com";
